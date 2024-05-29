d7246b147fb409f2083239ad2adbd1b252697028
arnav062008 <arnavpalawat@gmail.com>
2024-05-29 18:52:42 -0400

    added Flask and Firebase to script

--- jobs.py
+++ jobs.py
@@ -1,26 +1,46 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
 class Job:
-    def __init__(self, id, title, company, description, link):
+    def __init__(self, id, title, company, description, link, prestige):
         self.id = id
         self.title = title
         self.company = company
         self.description = description
         self.link = link
+        self.prestige = prestige
 
     def __repr__(self):
-        return f"Job(id={self.id}, title={self.title}, company={self.company}, description={self.description}, link={self.link})"
+        return (f"Job(id={self.id}, title={self.title}, company={self.company}, "
+                f"description={self.description}, link={self.link}, prestige={self.prestige})")
 
     def display(self):
         return (f"Job ID: {self.id}\n"
                 f"Title: {self.title}\n"
                 f"Company: {self.company}\n"
                 f"Description: {self.description}\n"
-                f"Link: {self.link}")
+                f"Link: {self.link}\n"
+                f"Prestige: {self.prestige}")
 
     def toMap(self):
         return {
-            "id": self.id,
+            "id" : self.id,
             "title": self.title,
             "company": self.company,
             "description": self.description,
-            "link": self.link
+            "link": self.link,
+            "prestige": self.prestige
         }
+
+    def firestoreAdd(self):
+        db = firestore.client()
+        doc_ref = db.collection('jobs').document(self.id)
+        doc = doc_ref.get()
+        if doc.exists:
+            print("Already existing document")
+        else:
+            doc_ref.set(
+                self.toMap()
+            )
--- main.py
+++ main.py
@@ -1,51 +1,101 @@
+import os
+
+import firebase_admin
+from firebase_admin import credentials
+from flask import Flask, jsonify, url_for
+from groq import Groq
 from jobspy import scrape_jobs
 from jobs import Job
-import csv
 
-# Function to filter jobs based on criteria
-def filter_jobs(jobs_list):
-    filtered_jobs = []
-    exclude_keywords = ["bachelor", "accredited", "college", "undergraduate", "major", "mba", "degree", "Fulltime", "Full-time", "full time", "Full-Time", "Full-time", "Full Time", "related experience", "CEO", "ceo", "CFO", "cfo", " ms ", " MS ", "PhD", "+ years"]
-    include_keywords = ["intern", "high school"]
-    include_phrases = ["high school", "highschool", "internship"]
-    exclude_phrases = ["college", "university"]
-    exclude_titles = ["CEO", "CFO", "CTO", "Chief", "Director", "Manager", "Senior"]  # Titles indicating big shot roles
+app = Flask(__name__)
+cred = credentials.Certificate('intern-b54ae-firebase-adminsdk-f0340-bc43dd3fdf.json')
+firebase_admin.initialize_app(cred)
 
-    for job in jobs_list:
-        description_lower = job.description.lower()
-        title_lower = job.title.lower()
 
-        # Check if the job description and title meet the criteria
-        if all(keyword not in description_lower for keyword in exclude_keywords) \
-                and any(keyword in description_lower for keyword in include_keywords) \
-                and any(phrase in description_lower for phrase in include_phrases) \
-                and all(phrase not in description_lower for phrase in exclude_phrases) \
-                and all(title not in title_lower for title in exclude_titles):
-            filtered_jobs.append(job)
+@app.route('/')
+def get_jobs():
+    global prestige, job
+    location = "Massachusetts"
+    field = "Accounting"
+    os.environ["GROQ_API_KEY"] = "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"
 
-    return filtered_jobs
+    # Function to filter jobs based on criteria
 
-# Scrape jobs
-jobs = scrape_jobs(
-    site_name=["indeed"],
-    search_term="Accounting",
-    location="Massachusetts",
-    results_wanted=100000,
-    country_indeed='USA',
-    hours_old=10000
-)
+    def filter_jobs(jobs_list):
+        filtered_jobs = []
+        exclude_keywords = ["bachelor", "accredited", "college", "undergraduate", "major", "mba", "degree", "Fulltime",
+                            "Full-time", "full time", "Full-Time", "Full-time", "Full Time", "related experience",
+                            "CEO", "ceo", "CFO", "cfo", " ms ", " MS ", "PhD", "+ years"]
+        include_keywords = ["intern", "high school"]
+        include_phrases = ["high school", "highschool", "internship"]
+        exclude_phrases = ["college", "university"]
+        exclude_titles = ["CEO", "CFO", "CTO", "Chief", "Director", "Manager", "Senior"]
 
-# Convert scraped jobs into Job objects
-jobs_list = [Job(row['id'], row['title'], row['company'], row['description'], row['job_url']) for _, row in jobs.iterrows() if row["job_type"] != "fulltime"]
+        for job in jobs_list:
+            description_lower = job.description.lower()
+            title_lower = job.title.lower()
 
-print(f"Found {len(jobs_list)} jobs")
+            # Check if the job description and title meet the criteria
+            if all(keyword not in description_lower for keyword in exclude_keywords) \
+                    and any(keyword in description_lower for keyword in include_keywords) \
+                    and any(phrase in description_lower for phrase in include_phrases) \
+                    and all(phrase not in description_lower for phrase in exclude_phrases) \
+                    and all(title not in title_lower for title in exclude_titles):
+                filtered_jobs.append(job)
 
-# Filter jobs
-filtered_jobs = filter_jobs(jobs_list)
+        return filtered_jobs
 
-print(f"Number of filtered jobs: {len(filtered_jobs)}")
+    # Scrape jobs
+    jobs = scrape_jobs(
+        site_name=["indeed"],
+        search_term=field + " Intern",
+        location=location,
+        results_wanted=100000,
+        country_indeed='USA',
+        hours_old=10000
+    )
 
-# Print filtered jobs
-for job in filtered_jobs:
-    print(job.title)
-    print(job.link)
+    jobs_list = [Job(row['id'], row['title'], row['company'], row['description'], row['job_url'], 0) for _, row in
+                 jobs.iterrows() if row["job_type"] != "fulltime"]
+
+    print(f"Found {len(jobs_list)} jobs")
+
+    # Filter jobs
+    filtered_jobs = filter_jobs(jobs_list)
+
+    print(f"Number of filtered jobs: {len(filtered_jobs)}")
+
+    # Prepare the response
+    final_job_list = []
+    job_data = []
+    api_key = os.getenv("GROQ_API_KEY")
+    client = Groq(api_key=api_key)
+    for job in filtered_jobs:
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": "Rate out of 5 the prestige of use this key: (fortune 100 = 5) (fortune 500 = 4) (Mid sized = 3 & 2) (small local shop = 1) for " + job.company + "using only 1 number nothing else",
+                }
+            ],
+            model="llama3-8b-8192",
+        )
+        prestige = chat_completion.choices[0].message.content
+        job.prestige = prestige
+        final_job_list.append(job)
+        job_data.append({
+            'title': job.title,
+            'link': job.link,
+            'prestige': prestige,
+        })
+    for job in final_job_list:
+        job.firestoreAdd()
+    # Get the endpoint URL
+    jobs_endpoint = url_for('get_jobs', _external=True)
+    print(f"Endpoint URL: {jobs_endpoint}")
+
+    return jsonify(job_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
